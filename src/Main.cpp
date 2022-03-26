9f6663cb073630bdae5add1effe751f4ee6f346e
Paulchch <83615379+Paulchch@users.noreply.github.com>
2022-03-26 00:12:35 -0600

    Falta Clases derivadas de Employee-->Contractor y FullTimeEmployee      * así como las interfaces IworkingFromOffice que hereda de Contractor      * y IWorkingFromHome que hereda de Contractor y FullTimeEmployee      * Para así ya poder empezar el laboratorio 2

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,12 @@
 #include <iostream>
 
 int main() {
-
+    /*
+     * Falta Clases derivadas de Employee-->Contractor y FullTimeEmployee
+     * así como las interfaces IworkingFromOffice que hereda de Contractor
+     * y IWorkingFromHome que hereda de Contractor y FullTimeEmployee
+     * Para así ya poder empezar el laboratorio 2
+     * */
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
